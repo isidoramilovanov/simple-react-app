acad05b7c9ce9d5e09332f5d1afc8e405590d5d2
isidoramilovanov <isidoramilovanov@gmail.com>
2019-11-08 10:26:27 +0100

    Updates

---
+++ precache-manifest.3ffe15aab4b55ad1ef1b486c033dc165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb91ee8caf4d60e1057dae4d5540cc1",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "1859d98565ab99117a60",
+    "url": "/simple-react-app/static/css/main.51702eed.chunk.css"
+  },
+  {
+    "revision": "c635db4c1b712390374b",
+    "url": "/simple-react-app/static/js/2.a4d5c8ba.chunk.js"
+  },
+  {
+    "revision": "1859d98565ab99117a60",
+    "url": "/simple-react-app/static/js/main.e4b87341.chunk.js"
+  },
+  {
+    "revision": "72cef20164bcd5534bf1",
+    "url": "/simple-react-app/static/js/runtime-main.bfabc0d7.js"
+  }
+]);
